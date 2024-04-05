78477c92647983709bb6ffe830096b5afb78a15f
Dev Bhandari <devbh789@gmail.com>
2024-04-05 14:59:25 +0530

    Added env variables

---
+++ src/config/server.config.js
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const API_ENDPOINT = process.env.API_ENDPOINT;
+
+export { API_ENDPOINT };
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { API_ENDPOINT } from "../config/server.config";
 
 // const baseURL = "http://localhost:3000/api/v1";
-const baseURL = "https://cu-threads-api.vercel.app/api/v1"; // Update with your backend URL
+const baseURL = `${API_ENDPOINT}/api/v1`; // Update with your backend URL
 
 const api = axios.create({
     baseURL,
