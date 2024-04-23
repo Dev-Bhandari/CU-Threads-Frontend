bdee595af075d860080fc25f7d8ec387ee3f6a0d
Dev Bhandari <devbh789@gmail.com>
2024-04-23 20:24:26 +0530

    Base url changed

--- src/utils/api/index.api.js
+++ src/utils/api/index.api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { API_ENDPOINT } from "../../config/server.config";
 
-const baseURL = "http://localhost:3000/api/v1";
-// const baseURL = `${API_ENDPOINT}/api/v1`;
+// const baseURL = "http://localhost:3000/api/v1";
+const baseURL = `${API_ENDPOINT}/api/v1`;
 
 const api = axios.create({
     baseURL,
